68841d4a357e943fd22d28f22321ee0237e75c78
aidanhoang1 <aidanhoang@csu.fullerton.edu>
2026-09-03 09:31:29 -0700

    Create M1A3.py

---
+++ M1A3.py
@@ -0,0 +1,19 @@
+# Name: Aidan Hoang
+# Student ID:  836253799
+# Section: 18254
+# Assignment: Module 1 Assignment 3
+
+first_int = int(input("What is the integer? "))
+second_float = float(input("What is the float? "))
+
+message_str = f"{first_int} + {second_float} is {first_int + second_float}"
+print(message_str)
+
+message_str = f"{first_int} - {second_float} is {first_int - second_float}"
+print(message_str)
+
+message_str = f"{first_int} * {second_float} is {first_int * second_float}"
+print(message_str)
+
+message_str = f"{first_int} / {second_float} is {first_int / second_float}"
+print(message_str)
