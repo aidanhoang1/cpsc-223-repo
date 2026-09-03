2a1864d57e1cb7edb29593be0a02be441be1becd
aidanhoang1 <aidanhoang@csu.fullerton.edu>
2026-09-03 09:26:41 -0700

    Update M1A2.py

--- M1A2.py
+++ M1A2.py
@@ -1,1 +1,7 @@
+# Name: Aidan Hoang
+# Student ID:  836253799
+# Section: 18254
+# Assignment: Module 1 Assignment 2
 
+name_str = input("What is your name? ")
+print(f"It's dangerous to go alone {name_str}, take this!")
