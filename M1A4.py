4dbc0162c8d176b1b28cf5e368fcccabb334ae41
aidanhoang1 <aidanhoang@csu.fullerton.edu>
2026-09-03 09:43:06 -0700

    Update M1A4.py

--- M1A4.py
+++ M1A4.py
@@ -1,1 +1,11 @@
+# Name: Aidan Hoang
+# Student ID:  836253799
+# Section: 18254
+# Assignment: Module 1 Assignment 4
 
+catchphrase_str = input("Please enter a catchphrase")
+
+print(f"[{catchphrase_str.upper()}]")
+print(f"[{catchphrase_str.lower()}]")
+print(f"[{catchphrase_str.lstrip()}]")
+print(f"[{catchphrase_str.rstrip()}]")
