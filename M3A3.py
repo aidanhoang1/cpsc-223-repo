9981dca5a55293c8d5e7a76601247a5b5f6748e1
aidanhoang1 <aidanhoang@csu.fullerton.edu>
2026-09-17 09:05:27 -0700

    Create M3A3.py

---
+++ M3A3.py
@@ -0,0 +1,11 @@
+# Name: Aidan Hoang
+# Student ID: 836253799
+# Section: 18254
+# Assignment: Module 3 Assignment 3
+
+num = int(input("Please enter an integer: "))
+
+if (num % 2 == 0):
+    print("Even")
+else:
+    print("Odd")
